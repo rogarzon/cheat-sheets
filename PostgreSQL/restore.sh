02fdd51e7ad208e9abfcd76571913fc34da37fb8
Reyter Onán Garzón Cintra <rogarzonc87@gmail.com>
2025-04-11 12:25:26 -0300

    [IMP][][] - PostgreSQL/README.md - restore

--- PostgreSQL/restore.sh
+++ PostgreSQL/restore.sh
@@ -10,3 +10,5 @@ FILE_NAME="grp_test.2024-05-09-test.sql.gz"
 cd ${FILE_PATH}
 
 zcat ${FILE_NAME} | PGPASSWORD=${DB_PWD} psql --username=${DB_USER} --host=${HOST} --port=${PORT} ${DB}
+
+#PGPASSWORD=${DB_PWD} pg_restore --verbose --username=${DB_USER} --dbname=${DB} --host=${HOST} --jobs=6  --no-owner --no-privileges --format=directory  ${FILE_PATH}
